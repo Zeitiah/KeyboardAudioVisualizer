f614c27c172808ed37de11b6b41b2555c3ee4572
Zeitiah <awesomemeandawesomeyou@gmail.com>
2021-03-12 13:18:30 -0600

    Idea for this branch is to pipe a sample of sound and determine its pitch, then write to the leds

--- new_frequency.py
+++ new_frequency.py
@@ -1,15 +1,15 @@
 #!/bin/python
 import aubio
 import subprocess 
+import sys
 from time import sleep
 
 def write_to_input(path, switch):
     x = open(path,'w')
     x.write(switch)
     x.close()
-def get_pitches(path):
-    
-    src = aubio.source(path)
+def get_pitches():
+    src = aubio.source(sys.stdin)
     hop_size = src.hop_size
     pitches = []
     pitch_object = aubio.pitch(samplerate=src.samplerate)
@@ -24,7 +24,9 @@ def get_pitches(path):
 
 if __name__ == '__main__':
     # Length is 24291
-    for i in get_pitches('036-XenobladeChroniclesOST-GaurPlain.mp3'):
+    print(sys.stdin)
+    print(len(get_pitches()))
+    for i in get_pitches():
         if i < 25:
             write_to_input('/sys/class/leds/input17::scrolllock/brightness', '0') 
             write_to_input('/sys/class/leds/input17::capslock/brightness', '0') 
