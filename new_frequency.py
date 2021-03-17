8c534d3ce7f947df136eb530917243dcd862b20a
Zeitiah <awesomemeandawesomeyou@gmail.com>
2021-03-17 14:43:38 -0500

    Wed Mar 17 02:43:38 PM CDT 2021

--- new_frequency.py
+++ new_frequency.py
@@ -9,18 +9,42 @@ def write_to_input(path, switch):
     x.write(switch)
     x.close()
 def get_pitches(path):
-    
     src = aubio.source(path)
     hop_size = src.hop_size
-    pitches = []
+    pyaudio_object = pyaudio.PyAudio()
+    stream = pyaudio_object.open(format=pyaudio.paFloat32, channels=src.channels, rate=src.samplerate, output=True)
     pitch_object = aubio.pitch(samplerate=src.samplerate)
 
     while True:
         samples, read = src()
+        byte_samples = bytes()
+        for i in samples:
+            byte_samples += bytes(i)
+        stream.write(byte_samples)
         pitch = pitch_object(samples)
-        pitches += [pitch[0]]
-        if read < hop_size:
+        if pitch[0] < 25:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        elif pitch[0] < 440:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        elif pitch[0] < 1100:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        else:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '1') 
+
+        if read < src.hop_size:
             break
+    stream.stop_stream()
+    stream.close()
+    pyaudio_object.terminate()
+    print('Finished get_pitches')
     return pitches
 
 if __name__ == '__main__':
@@ -42,5 +66,5 @@ if __name__ == '__main__':
             write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
             write_to_input('/sys/class/leds/input17::capslock/brightness', '1') 
             write_to_input('/sys/class/leds/input17::numlock/brightness', '1') 
-        sleep(0.1)
+        sleep(0.01)
 
