ded481120e68c3b2d020b81c6a81c69568cf4ca1
Zeitiah <awesomemeandawesomeyou@gmail.com>
2021-03-17 14:58:25 -0500

    The currently used method

--- frequency.py
+++ frequency.py
@@ -1,34 +1,70 @@
 #!/bin/python
-import sys
-from subprocess import call, Popen, PIPE
+import aubio
+import subprocess 
+import pyaudio
+from time import sleep
 
-def write_to_file(var, path):
-    x = open(path, 'w')
-    x.write(var)
+def write_to_input(path, switch):
+    x = open(path,'w')
+    x.write(switch)
     x.close()
+def get_pitches(path):
+    src = aubio.source(path)
+    hop_size = src.hop_size
+    pyaudio_object = pyaudio.PyAudio()
+    stream = pyaudio_object.open(format=pyaudio.paFloat32, channels=src.channels, rate=src.samplerate, output=True)
+    pitch_object = aubio.pitch(samplerate=src.samplerate)
 
-txt = 'out.txt'
-while True:
-    p = Popen(['tail', '-1',txt],shell=False, stdout=PIPE)
-    ooutput = str(p.stdout.read())
-    boutput = ooutput.replace(r'\n','').replace('b','').replace("'","")
-    try:
-        output = int(boutput.split()[1][:boutput.index('.')])
-    except:
-        continue
-    print(output)
-    if output < 440:
-        write_to_file('0','/sys/class/leds/input17::numlock/brightness')
-        write_to_file('0', '/sys/class/leds/input17::capslock/brightness')
-        write_to_file('1', '/sys/class/leds/input17::scrolllock/brightness')
-    elif output < 1000:
-        write_to_file('0','/sys/class/leds/input17::numlock/brightness')
-        write_to_file('1', '/sys/class/leds/input17::capslock/brightness')
-        write_to_file('1', '/sys/class/leds/input17::scrolllock/brightness')
-    else:
-        write_to_file('1','/sys/class/leds/input17::numlock/brightness')
-        write_to_file('1', '/sys/class/leds/input17::capslock/brightness')
-        write_to_file('1', '/sys/class/leds/input17::scrolllock/brightness')
+    while True:
+        samples, read = src()
+        byte_samples = bytes()
+        for i in samples:
+            byte_samples += bytes(i)
+        stream.write(byte_samples)
+        pitch = pitch_object(samples)
+        if pitch[0] < 25:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        elif pitch[0] < 440:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        elif pitch[0] < 1100:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        else:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '1') 
 
+        if read < src.hop_size:
+            break
+    stream.stop_stream()
+    stream.close()
+    pyaudio_object.terminate()
+    print('Finished get_pitches')
+    return pitches
 
+if __name__ == '__main__':
+    # Length is 24291
+    for i in get_pitches('036-XenobladeChroniclesOST-GaurPlain.mp3'):
+        if i < 25:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        elif i < 440:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '0') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        elif i < 1100:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '0') 
+        else:
+            write_to_input('/sys/class/leds/input17::scrolllock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::capslock/brightness', '1') 
+            write_to_input('/sys/class/leds/input17::numlock/brightness', '1') 
+        sleep(0.01)
 
